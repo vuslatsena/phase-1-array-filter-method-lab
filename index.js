a2f04982ec047e0689ffd0ae9722ec3a5a8d30b6
vuslatsena <vuslatsena23@gmail.com>
2023-04-29 12:25:42 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+
+const drivers = [
+  { name: 'Vuslat', hometown: 'Pittsburgh' },
+  { name: 'Sena', hometown: 'New York' },
+  { name: 'Emre', hometown: 'Cleveland' },
+  { name: 'Dobby', hometown: 'Los Angeles' },
+  { name: 'Harry', hometown: 'Tampa Bay' }
+];
+
+function findMatching(drivers, query) {
+  return drivers.filter(driver => driver.toLowerCase() === query.toLowerCase());
+}
+
+function fuzzyMatch(drivers, query) {
+  return drivers.filter(driver => driver.toLowerCase().startsWith(query.toLowerCase()));
+}
+
+function matchName(drivers, query) {
+  return drivers.filter(driver => driver.name.toLowerCase() === query.toLowerCase());
+}
